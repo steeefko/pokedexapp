0d84f82e97ab308adf64b46afdf891626e8d993e
Stefan Milenkovic <stefanmilenkovic@Stefans-MacBook-Air.local>
2016-04-16 12:26:25 +0200

    Adding project files

---
+++ pokedexapp/PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  pokedexapp
+//
+//  Created by Stefan Milenkovic on 4/16/16.
+//  Copyright © 2016 swiftkoder. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
